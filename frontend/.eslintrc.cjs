d0e28658639d7e4a002af7934da9f16323d40d96
Marc Smalbrugge <marc.smalbrugge@gmail.com>
2026-09-20 11:07:32 +0200

    setup linting and formatting

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -5,14 +5,20 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
     ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
 }
